5e98c18f8f4a5378f944aa221b4a0d1ddf8173f1
Nyquinho <148580491+Nyquinho@users.noreply.github.com>
2024-10-15 15:43:04 -0300

    f

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'programação',
+    'O que é Phyton?',
+    'O Phyton é uma linguagem de programação'
+)
+
+criaCartao(
+    'geografia',
+    'Qual a capital da frança?',
+    'a capital da frança é Paris'
+)
+
+criaCartao(
+    'programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que excuta alguma tarefa'
+)
+
+criaCartao(
+    'lingua inglesa',
+    'Como se diz oi em inglês?',
+    'oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'biologia',
+    'Quem é considerado o pai da genética?',
+    'Gregor mendel'
+)
+
+criaCartao(
+    'matemática',
+    'Qual é o valor de Pi?',
+    '3,14'
+)
