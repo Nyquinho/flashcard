f5102d0685b426e0aa55b92f4f298a75dd6abed6
Nyquinho <148580491+Nyquinho@users.noreply.github.com>
2024-11-05 15:35:10 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -6,14 +6,14 @@ criaCartao(
 
 criaCartao(
    'Biologia',
-    'Como funciona a Primeira Lei de Mendel em um exemplo simples?',
-    'Cruzando plantas de ervilha amarelas (AA) com verdes (aa), a primeira geração (F1) terá sementes amarelas (Aa). Na segunda geração (F2), a proporção será 3 amarelas para 1 verde.'
+    'O que diz a primeira lei de mendel?',
+    'Cada carater é condicionado por um par de fatores.'
     )
 
 criaCartao(
     'Biologia',
-    'Como a ideia de segregação independente se relaciona com a Primeira Lei de Mendel?',
-    'A Segunda Lei de Mendel aborda a segregação independente de diferentes características. A Primeira Lei também implica que os genes para uma característica se separam independentemente durante a formação dos gametas.')
+    'O que é um gene?',
+    'Unidade fundamental da hereditariedade.')
 
 criaCartao(
     'Biologia',
@@ -23,30 +23,30 @@ criaCartao(
 
 criaCartao(
     'Biologia',
-    'Como Mendel descobriu a Segunda Lei?',
-    'Mendel, ao estudar ervilhas em cruzamentos diíbridos, notou que a herança de uma característica não influenciava a outra. Assim, ele concluiu que os alelos de diferentes genes se segregam independentemente.'
+    'O que é um cariotipo?,
+    'Conjunto de cromossomos de uma especie.'
 )
 
 criaCartao(
     'Biologia',
-    'Qual é a diferença entre Genótipo e fenótipo?',
-    'Genótipo é a composição genética de um organismo, representando a combinação de alelos para um gene. Fenótipo, por outro lado, são as características observáveis resultantes da interação entre o genótipo e o ambiente, como cor, forma e comportamento.'
+    'O que é a meiose?',
+    'Tipo especial de divisão celular voltada a produção de gametas.'
 )
 
 criaCartao(
     'Biologia',
-    'Oque é Co-odominância?',
-    'Co-dominância é um padrão de herança onde ambos os alelos de um par são expressos igualmente no fenótipo. Um exemplo é o tipo sanguíneo AB, que combina características dos alelos A e B.'
+    'O que é especiação?',
+    'Mecanismo pelo qual surge novas especies.'
 )
 
 criaCartao(
     'Biologia',
-    'Oque é Heredogramas?',
-    'Heredogramas são representações gráficas que mostram a herança de características genéticas em famílias ao longo de gerações, permitindo identificar padrões de herança e a presença de doenças genéticas.'
+    'Cite exemplos de de especiação',
+    'Alopátrica, Simpátrica, Parapátrica e peripátrica.'
 )
 
 criaCartao(
     'Biologia',
-    'Considere uma planta de ervilha que é heterozigota para duas características: cor da semente (amarela é dominante sobre verde) e forma da semente (lisa é dominante sobre rugosa). Qual será a proporção fenotípica esperada na geração F2?',
-    '9:3:3:1 (9 amarelas lisas : 3 amarelas rugosas : 3 verdes lisas : 1 verde rugosa).'
+    'O que a meiose resolve?',
+    'Resolve o paradigma da manutenção da carga genética.'
 )
